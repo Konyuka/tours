337f18b1490f5ab80d62bcdd45d41bbd76e2595e
Michael Saiba <michaelsaiba84@gmail.com>
2023-03-02 11:45:37 +0300

    demo

---
+++ index.php
@@ -0,0 +1,520 @@
+<?php include './components/head.html'; ?>
+
+    <div class="hero">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-7">
+            <div class="intro-wrap">
+              <h1 class="mb-5">
+                <span class="d-block">Let's Enjoy Your</span> Trip In
+                <span class="typed-words"></span>
+              </h1>
+
+              <div class="row">
+                <div class="col-12">
+                  <form class="form">
+                    <div class="row mb-2">
+                      <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                        <select
+                          name=""
+                          id=""
+                          class="form-control custom-select"
+                        >
+                          <option value="">Destination</option>
+                          <option value="">Masai Mara National Reserve</option>
+                          <option value="">Mombasa</option>
+                          <option value="">Amboseli National Park</option>
+                          <option value="">Lamu Island</option>
+                          <option value="">Lake Nakuru National Park</option>
+                          <option value="">Tsavo National Park</option>
+                          <option value="">Samburu National Reserve</option>
+                        </select>
+                      </div>
+
+                      <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-5">
+                        <input
+                          type="text"
+                          class="form-control"
+                          placeholder="# of People"
+                        />
+                      </div>
+                    </div>
+                    <div class="row align-items-center">
+                      <div class="col-sm-12 col-md-6 mb-3 mb-lg-0 col-lg-4">
+                        <input
+                          type="submit"
+                          class="btn btn-primary btn-block"
+                          value="Search"
+                        />
+                      </div>
+                      <div class="col-lg-8">
+                        <label class="control control--checkbox mt-3">
+                          <span class="caption">Save this search</span>
+                          <input type="checkbox" checked="checked" />
+                          <div class="control__indicator"></div>
+                        </label>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-5">
+            <div class="slides">
+              <img
+                src="images/hero-slider-4.jpg"
+                alt="Image"
+                class="img-fluid active"
+              />
+              <img
+                src="images/hero-slider-2.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+              <img
+                src="images/hero-slider-3.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+              <img
+                src="images/hero-slider-4.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+              <img
+                src="images/hero-slider-5.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="untree_co-section">
+      <div class="container">
+        <div class="row mb-5 justify-content-center">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-title text-center mb-3">Our Services</h2>
+            <p>
+              Welcome to our touring website! We offer a range of services to
+              make your travel experience as enjoyable and stress-free as
+              possible. <br> <br>
+			  Our goal is to help you create unforgettable memories
+              and have a hassle-free travel experience. We provide a variety of
+              services such as booking accommodations, arranging transportation,
+              planning itineraries, and organizing guided tours. Our team of
+              experienced professionals are passionate about travel and are
+              dedicated to ensuring that you have an amazing time exploring the
+              world. <br> <br>
+			  Whether you are looking for a relaxing beach vacation, an
+              adventurous safari, or a cultural city break, we have got you
+              covered.
+			  We work with trusted partners to provide you with the
+              best possible travel options at competitive prices. <br> <br>
+			  Our website is
+              designed to make booking and planning your trip as easy as
+              possible. You can browse through our various destinations, select
+              your preferred travel dates, and customize your trip according to
+              your interests and budget. <br> <br>
+			  Thank you for choosing our touring
+              website. We are excited to be a part of your travel journey and
+              look forward to helping you plan your dream vacation.
+            </p>
+          </div>
+        </div>
+        <div class="row align-items-stretch">
+          <div class="col-lg-4 order-lg-1">
+            <div class="h-100">
+              <div class="frame h-100">
+                <div
+                  class="feature-img-bg h-100"
+                  style="background-image: url('images/hero-slider-2.jpg')"
+                ></div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-6 col-sm-6 col-lg-4 feature-1-wrap d-md-flex flex-md-column order-lg-1"
+          >
+            <div class="feature-1 d-md-flex">
+              <div class="align-self-center">
+                <span class="flaticon-house display-4 text-primary"></span>
+                <h3>Beautiful Destinations</h3>
+              </div>
+            </div>
+
+            <div class="feature-1">
+              <div class="align-self-center">
+                <span class="flaticon-restaurant display-4 text-primary"></span>
+                <h3>Restaurants & Cafe</h3>
+              </div>
+            </div>
+          </div>
+
+          <div
+            class="col-6 col-sm-6 col-lg-4 feature-1-wrap d-md-flex flex-md-column order-lg-3"
+          >
+            <div class="feature-1 d-md-flex">
+              <div class="align-self-center">
+                <span class="flaticon-mail display-4 text-primary"></span>
+                <h3>Easy Booking</h3>
+              </div>
+            </div>
+
+            <div class="feature-1 d-md-flex">
+              <div class="align-self-center">
+                <span class="flaticon-phone-call display-4 text-primary"></span>
+                <h3>24/7 Support</h3>
+                <!-- <p class="mb-0">
+                  Even the all-powerful Pointing has no control about the blind
+                  texts.
+                </p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="untree_co-section count-numbers py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div class="counter-wrap">
+              <div class="counter">
+                <span class="" data-number="193">0</span>
+              </div>
+              <span class="caption">No. of Travels</span>
+            </div>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div class="counter-wrap">
+              <div class="counter">
+                <span class="" data-number="892">0</span>
+              </div>
+              <span class="caption">No. of Clients</span>
+            </div>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div class="counter-wrap">
+              <div class="counter">
+                <span class="" data-number="65">0</span>
+              </div>
+              <span class="caption">No. of Employees</span>
+            </div>
+          </div>
+          <div class="col-6 col-sm-6 col-md-6 col-lg-3">
+            <div class="counter-wrap">
+              <div class="counter">
+                <span class="" data-number="82">0</span>
+              </div>
+              <span class="caption">No. of Countries</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="untree_co-section">
+      <div class="container">
+        <div class="row text-center justify-content-center mb-5">
+          <div class="col-lg-7">
+            <h2 class="section-title text-center">Popular Destination</h2>
+          </div>
+        </div>
+
+        <div class="owl-carousel owl-3-slider">
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-1.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>Pragser Wildsee</h3>
+                <span class="location">Italy</span>
+              </div>
+              <img
+                src="images/hero-slider-1.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-2.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>Oia</h3>
+                <span class="location">Greece</span>
+              </div>
+              <img
+                src="images/hero-slider-2.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-3.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>Perhentian Islands</h3>
+                <span class="location">Malaysia</span>
+              </div>
+              <img
+                src="images/hero-slider-3.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-4.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>Rialto Bridge</h3>
+                <span class="location">Italy</span>
+              </div>
+              <img
+                src="images/hero-slider-4.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-5.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>San Francisco, United States</h3>
+                <span class="location">United States</span>
+              </div>
+              <img
+                src="images/hero-slider-5.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+
+          <div class="item">
+            <a
+              class="media-thumb"
+              href="images/hero-slider-1.jpg"
+              data-fancybox="gallery"
+            >
+              <div class="media-text">
+                <h3>Lake Thun</h3>
+                <span class="location">Switzerland</span>
+              </div>
+              <img
+                src="images/hero-slider-2.jpg"
+                alt="Image"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div> -->
+
+    <div class="untree_co-section testimonial-section mt-5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-7 text-center">
+            <h2 class="section-title text-center mb-5">Testimonials</h2>
+
+            <div class="owl-single owl-carousel no-nav">
+              <div class="testimonial mx-auto">
+                <!-- <figure class="img-wrap">
+                  <img
+                    src="images/person_2.jpg"
+                    alt="Image"
+                    class="img-fluid"
+                  />
+                </figure> -->
+                <h3 class="name">Adam Mugithi</h3>
+                <blockquote>
+                  <p>
+                    I recently went on a tour organized by this company, and I couldn't have been more impressed. The itinerary was well-planned, and the tour guides were knowledgeable and friendly. I highly recommend this company to anyone looking for a memorable and stress-free travel experience.
+                  </p>
+                </blockquote>
+              </div>
+
+              <div class="testimonial mx-auto">
+                <figure class="img-wrap">
+                <h3 class="name">Lukas Daudi</h3>
+                <blockquote>
+                  <p>
+                    I had an amazing time on my trip, thanks to this tour company. They took care of all the details, from transportation to accommodations, and everything in between. The tour guides were fantastic and provided valuable insights into the local culture and history. I can't wait to book another trip with them!
+                  </p>
+                </blockquote>
+              </div>
+
+              <div class="testimonial mx-auto">
+                <h3 class="name">Kayla Mueni</h3>
+                <blockquote>
+                  <p>
+                    This tour company exceeded all my expectations. The itinerary was perfectly balanced, with a mix of cultural experiences and outdoor activities. The tour guides were attentive and went above and beyond to ensure that everyone had a great time. I highly recommend this company for anyone looking for a unique and unforgettable travel experience.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="untree_co-section">
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-lg-6">
+            <figure class="img-play-video">
+              <a
+                id="play-video"
+                class="video-play-button"
+                href="https://www.youtube.com/watch?v=mwtbEGNABWU"
+                data-fancybox
+              >
+                <span></span>
+              </a>
+              <img
+                src="images/hero-slider-3.jpg"
+                alt="Image"
+                class="img-fluid rounded-20"
+              />
+            </figure>
+          </div>
+
+          <div class="col-lg-5">
+            <h2 class="section-title text-left mb-4">
+              Take a look at Tour Video
+            </h2>
+            <p>
+              Are you ready for a wild ride? Well, you've come to the right place! Touring in Kenya is like nothing you've ever experienced before. From dodging potholes on the roads to spotting the "Big Five" in the wild, you never know what kind of adventure you're going to get.
+
+				And let's not forget about the unexpected roadside stops to take a picture with a friendly camel or to buy some roasted maize from a local vendor. It's all part of the experience, right?
+
+				But don't worry, we've got you covered. 
+            </p>
+
+            <p class="mb-4">
+              Our expert guides know all the ins and outs of touring in Kenya, and they'll make sure you have a blast while staying safe and comfortable.
+
+				So come on, let's go on a safari, hike through lush forests, and lounge on pristine beaches. Who knows, you might even learn a few words of Swahili along the way. Karibu Kenya! (That means welcome to Kenya, by the way)
+            </p>
+
+            <ul class="list-unstyled two-col clearfix">
+              <li>Outdoor recreation activities</li>
+              <li>Airlines</li>
+              <li>Car Rentals</li>
+              <li>Cruise Lines</li>
+              <li>Hotels</li>
+              <li>Railways</li>
+              <li>Travel Insurance</li>
+              <li>Package Tours</li>
+              <li>Insurance</li>
+              <li>Guide Books</li>
+            </ul>
+
+            <p><a href="#" class="btn btn-primary">Get Started</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="py-5 cta-section">
+      <div class="container">
+        <div class="row text-center">
+          <div class="col-md-12">
+            <h2 class="mb-2 text-white">
+              Lets you Explore the Best. Contact Us Now
+            </h2>
+            <p class="mb-4 lead text-white text-white-opacity">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Excepturi, fugit?
+            </p>
+            <p class="mb-0">
+              <a
+                href="booking.html"
+                class="btn btn-outline-white text-white btn-md font-weight-bold"
+                >Get in touch</a
+              >
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php include './components/footer.html'; ?>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/moment.min.js"></script>
+    <script src="js/daterangepicker.js"></script>
+
+    <script src="js/typed.js"></script>
+    <script>
+      KES(function () {
+        var slides = KES(".slides"),
+          images = slides.find("img");
+
+        images.each(function (i) {
+          KES(this).attr("data-id", i + 1);
+        });
+
+        var typed = new Typed(".typed-words", {
+          strings: [
+            "San Francisco.",
+            " Paris.",
+            " New Zealand.",
+            " Maui.",
+            " London.",
+          ],
+          typeSpeed: 80,
+          backSpeed: 80,
+          backDelay: 4000,
+          startDelay: 1000,
+          loop: true,
+          showCursor: true,
+          preStringTyped: (arrayPos, self) => {
+            arrayPos++;
+            console.log(arrayPos);
+            KES(".slides img").removeClass("active");
+            KES('.slides img[data-id="' + arrayPos + '"]').addClass("active");
+          },
+        });
+      });
+    </script>
+
+    <script src="js/custom.js"></script>
+  </body>
+</html>
